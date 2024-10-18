8bd495d5ba012de77057642b401ab49499f677ce
David Tomás <darktomas2045@gmail.com>
2024-10-18 09:23:53 +0100

    java: RESTful API

---
+++ java-bootcamp/src/test/java/me/david/java_bootcamp/JavaBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package me.david.java_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
